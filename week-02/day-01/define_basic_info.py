0e77f819c681c5ebb1db17f7f19cf4132d8acd26
Tofiz <tofi.zagon@gmail.com>
2017-09-11 14:36:19 +0200

    feat(define_basic_info.py) done

--- week-02/day-01/define_basic_info.py
+++ week-02/day-01/define_basic_info.py
@@ -3,3 +3,10 @@
 # Your age as an integer
 # Your height in meters as a double
 # Wether you are married or not as a boolean
+
+name = "Kristof Zagon"
+age = 27
+height = float(1.8)
+married = False
+
+print( "name: " + str(name) + ", age: " + str(age) + ", height: " + str(height) +", married: " + str(married))
