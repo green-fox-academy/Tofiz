49d55d301c29670a51bef8b4efe1aa72da66a106
Tofiz <tofi.zagon@gmail.com>
2017-09-11 16:11:06 +0200

    feat(mile_to_km_converter.py) done

--- week-02/day-01/mile_to_km_converter.py
+++ week-02/day-01/mile_to_km_converter.py
@@ -1,2 +1,11 @@
 # Write a program that asks for an integer that is a distance in kilometers,
 # then it converts that value to miles and prints it
+
+print("Wlecome, this is a mile to kilomter converter!")
+x = input( 'Please add a value in km: ')
+inmile = int(x) * 0.621371192
+print("Let'me see, is must be ", str(inmile) + " in mile.")
+
+# print("Welcome, this is a mile to kilomter converter!")
+# km = input("Distance in km: ")
+# print("Converted to miles:" , int(km) * 0.62)
