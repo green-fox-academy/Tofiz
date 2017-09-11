84f7c0ada015493c74f3038b6d1dc8823887faeb
Tofiz <tofi.zagon@gmail.com>
2017-09-11 12:16:57 +0200

    feat(hello_me.py) done

---
+++ week-02/day-01/file.py
@@ -0,0 +1,9 @@
+# student_count = 100
+# student_per_class = 20
+# is_student_ready: False
+
+# while student_count > 0:
+#         print('hello ', student_count)
+#         student_count -= 1
+# print( range(3))    
+print( int(1.9))
