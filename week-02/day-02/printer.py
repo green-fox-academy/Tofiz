1d5f690b57789a59a4c7866a2272808bcaa7858a
Tofiz <tofi.zagon@gmail.com>
2017-09-12 12:34:56 +0200

    feat(printer.py) done

---
+++ week-02/day-02/printer.py
@@ -0,0 +1,10 @@
+# - Create a function called `printer`
+#   which prints the input parameters
+#   (can have multiple number of arguments)
+
+n = (input("Gimme parameters :"))
+
+def printer(text):
+        print(text)
+
+printer(n)
