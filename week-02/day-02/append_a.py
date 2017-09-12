6749f87b218cf212a62d77f74b9ec368e19ab9f5
Tofiz <tofi.zagon@gmail.com>
2017-09-12 16:17:27 +0200

    feat(append_a.py) done

---
+++ week-02/day-02/append_a.py
@@ -0,0 +1,12 @@
+# - Create a variable named `nimals`
+#   with the following content: `["kuty", "macs", "cic"]`
+# - Add all elements an `"a"` at the end
+
+nimals = ["kuty", "macsk", "cic"]
+
+
+for i in range(3):
+    print(nimals[i] + "a")
+
+
+# for i in range(0, len(nimals)):
