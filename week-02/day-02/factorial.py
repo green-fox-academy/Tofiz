4b45627aa2b4a2fc05aa76c5d643c6b25171b3cf
Tofiz <tofi.zagon@gmail.com>
2017-09-12 12:29:50 +0200

    fix(factorial.py) done

--- week-02/day-02/factorial.py
+++ week-02/day-02/factorial.py
@@ -1,11 +1,13 @@
 # - Create a function called `factorio`
 #   that returns it's input's factorial
 
+n = int(input("Gimme a number: "))
+
 def factorio (num):
     x = 1
     for i in range(1, num+1):
         x *= i        
     return x
-number = factorio(5)
+number = factorio(n)
 
-print(number)
+print("The factoro: ", number)
