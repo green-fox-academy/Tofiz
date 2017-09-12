fe7397e50af9a76b9e62535563d40bda290a55dc
Tofiz <tofi.zagon@gmail.com>
2017-09-12 16:45:38 +0200

    feat(reverse.py) done

---
+++ week-02/day-02/reverse.py
@@ -0,0 +1,13 @@
+# - Create a variable named `aj`
+#   with the following content: `[3, 4, 5, 6, 7]`
+# - Reverse the order of the elements in `aj`
+# - Print the elements of the reversed `aj`
+
+
+one_line = ""
+aj = [3, 4, 5, 6, 7]
+
+for n in reversed(aj):
+    one_line = one_line + str(n) + ", "
+print (one_line)
+
