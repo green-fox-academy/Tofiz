51aacd42c3d523b21e1cc45a79853a924dbf6ba0
Tofiz <tofi.zagon@gmail.com>
2017-09-12 12:07:56 +0200

    feat(sum.py) done

--- week-02/day-02/sum.py
+++ week-02/day-02/sum.py
@@ -1,2 +1,22 @@
 # - Write a function called `sum` that sum all the numbers
 #   until the given parameter
+
+# def sum(user_input):
+#     for i = range
+#     return user_input
+
+# print(range())
+
+
+# n = int(input())
+
+def sum(num):
+    x = 0
+    for i in range(1, num):
+        x += i 
+    return x
+        # print(sum(x))
+        
+number = sum(9)
+
+print(number)
