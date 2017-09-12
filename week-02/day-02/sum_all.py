e7721005986908b6a5886c6b9120587917fbc308
Tofiz <tofi.zagon@gmail.com>
2017-09-12 16:38:25 +0200

    feat(sum_all.py) done

---
+++ week-02/day-02/sum_all.py
@@ -0,0 +1,13 @@
+# - Create a variable named `ai`
+#   with the following content: `[3, 4, 5, 6, 7]`
+# - Print the sum of the elements in `ai`
+
+ai = [3, 4, 5, 6, 7]
+
+x = 0
+for n in range(0,len(ai)):
+    x += ai[n]
+print(x)
+
+
+print (sum(ai))
