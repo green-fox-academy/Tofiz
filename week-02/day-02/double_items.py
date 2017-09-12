38f9dd0882ed30b7129661d7a3609dfcb93d8d7d
Tofiz <tofi.zagon@gmail.com>
2017-09-12 15:37:26 +0200

    feat(double_items.py) done

---
+++ week-02/day-02/double_items.py
@@ -0,0 +1,23 @@
+# - Create an array variable named `ag`
+#   with the following content: `[3, 4, 5, 6, 7]`
+# - Double all the values in the array
+
+one_line = ""
+ag = [3, 4, 5, 6, 7]
+
+for n in range(0,len(ag)):
+    ag[n] *= 2
+    one_line = one_line + str(ag[n]) + " "
+    
+print (one_line)
+
+    
+
+# def double(lst):
+#     x = 0
+#     for i in ag:
+#         x *= 2
+#     print x
+#     return lst
+
+# print double(ag)
