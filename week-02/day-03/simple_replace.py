4392a80f2edec3183cae7adfcd2248e80dc772db
Tofiz <tofi.zagon@gmail.com>
2017-09-13 10:34:31 +0200

    feat(simple_replace.py) done

---
+++ week-02/day-03/simple_replace.py
@@ -0,0 +1,10 @@
+example = "In a dishwasher far far away";
+
+# I would like to replace "dishwasher" with "galaxy" in this example
+# Please fix it for me!
+# Expected ouput: In a galaxy far far away
+
+
+example = example[:4] + " galaxy " + example[16:]
+
+print(example)
