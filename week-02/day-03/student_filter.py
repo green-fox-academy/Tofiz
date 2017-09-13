31fefed416fa33bff422f8fb585a4595d4f40778
Tofiz <tofi.zagon@gmail.com>
2017-09-13 15:42:14 +0200

    feat(student_filter.py):done.py

---
+++ week-02/day-03/student_filter.py
@@ -0,0 +1,27 @@
+students = [
+        {'name': 'Rezso', 'age': 9.5, 'candies': 2},
+        {'name': 'Gerzson', 'age': 10, 'candies': 1},
+        {'name': 'Aurel', 'age': 7, 'candies': 3},
+        {'name': 'Zsombor', 'age': 12, 'candies': 5}
+]
+
+# create a function that takes a list of students and prints:
+# - Who has got more candies than 4 candies
+
+def who_has_got_more_candies():
+        for i in range(len(students)):
+                if (students[i]['candies']) > 4:
+                    print(students[i])
+who_has_got_more_candies()
+
+# who_has_got_more_candies()
+
+# create a function that takes a list of students and prints: 
+#  - how many candies they have on average
+
+def students_candies():
+    avg=0
+    for i in range(len(students)):
+        avg += students[i]['candies']/len(students)
+    print(avg)       
+students_candies()
