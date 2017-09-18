395f42c56ceb7381e50fc4052bcc6e58bc833308
Tofiz <tofi.zagon@gmail.com>
2017-09-18 21:49:12 +0200

    remove animals_and_legs.py

--- week-02/day-01/animals_and_legs.py
+++
@@ -1,4 +0,0 @@
-# Write a program that asks for two integers
-# The first represents the number of chickens the farmer has
-# The seconf represents the number of pigs the farmer has
-# It should print how many legs all the animals have
