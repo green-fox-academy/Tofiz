dad88b82635cb926b6f46a02937cdfea4ee8db66
Tofiz <tofi.zagon@gmail.com>
2017-10-03 19:58:02 +0200

    feat(aircraft-carrier.py): done

--- week-04/day-02/aircraft-carrier.py
+++ week-04/day-02/aircraft-carrier.py
@@ -1,56 +1,67 @@
-class Aircraft:
-    def __init__(self, aircraft_type ):
-        self.aircraft_type = aircraft_type
-        self.ammo = 0
-        if aircraft_type == "f16":
-            self.max_ammo = 8
-            self.base_damage = 50
-        elif aircraft_type == "f35":
-            self.max_ammo = 12
-            self.base_damage = 50
-        
+class Aircraft(object):
+    	def __init__(self, aircraft_type):
+		self.aircraft_type = aircraft_type
+		self.ammo = 0
+		if aircraft_type == "f16":
+			self.max_ammo = 8
+			self.base_damage = 30
+		elif aircraft_type == "f35":	
+			self.max_ammo = 12
+			self.base_damage = 50
 
-    def fight(self):
-        current_ammo = ammo
-        self.ammo = 0
-        return base_damage * current_ammo
+	def fight(self):
+		current_ammo = self.ammo
+		self.ammo = 0	
+		return self.base_damage * current_ammo
 
+	def refill(self, amount_of_ammo):
+		if self.ammo == self.max_ammo:
+			return amount_of_ammo
+		elif self.ammo + amount_of_ammo < self.max_ammo:
+			self.ammo = self.ammo + amount_of_ammo
+			return 0
+		else:
+			self.ammo = self.max_ammo
+			return amount_of_ammo - self.max_ammo
 
-    def refill(self, amount_of_ammo):
-        if self.ammo == self.max_ammo:
-            return amount_of_ammo
-        if self.ammo + amount_of_ammo < self.max_ammo:
-            self.ammo = self.ammo + amount_of_ammo
-            return 0
-        else:
-                self.ammo = self.max_ammo
-                return amount_of_ammo - self.max_ammo
+	def get_type(self):
+		return self.aircraft_type		
 
-
-    def get_type(self):
-        return self.aircraft_type
-
-
-    def get_status():
-        return "Type: " + self.aircraft_type + "Ammo: " + self.ammo + "Base damage: " + self.base_damage + "All damage: " + str(self.ammo * self.base_damage)
+	def get_status(self):
+		return "Type: " + self.aircraft_type + " Ammo: " + str(self.ammo) + " Base damage: " + str(self.base_damage) +  " All damage:" + str(self.ammo * self.base_damage)
 
 class Carrier(object):
-    def __init__(self, ammo):
-        self.ammo = ammo
-        self.aircrafts = []
-        self.hp = hp
+	def __init__(self, ammo, hp):
+		self.ammo = ammo
+		self.aircrafts = []
+		self.hp = hp
 
+	def add_aircraft(self, aircraft_type):
+		self.aircrafts.append(Aircraft(aircraft_type))
 
-    def add_aircraft(self, aircraft_type):
-        aircrafts.append(Aircraft(aircraft_type))
-        
+	def fill(self):
+		if self.ammo == 0:
+			raise Exception("No ammo!")		
+		for aircraft in self.aircrafts:
+			if aircraft.aircraft_type == "f35":
+				self.ammo = aircraft.refill(self.ammo)
+		for aircraft in self.aircrafts:
+			self.ammo = aircraft.refill(self.ammo)
 
-    def fill(self):
-        if self.ammo == 0:
-            raise Exception("No ammo!")   
-        for aircraft in self.aircrafts:
-            if aircraft.aircraft_type == "f35":
-                self.ammo = aircraft.fill(self.ammo)    
-        for aircraft in self.aircrafts:
-            self.ammo = aircraft.refill(self.ammo)
-    
+	def fight(self, carrier):
+		sum_of_damage = 0
+		for aircraft in self.aircrafts:
+			sum_of_damage += aircraft.fight()
+		carrier.hp -= sum_of_damage
+		if carrier.hp < 0:
+			print("It's dead Jim :(")
+		
+	def get_status(self):
+		sum_of_damage = 0
+		for aircraft in self.aircrafts:
+			sum_of_damage += aircraft.base_damage * aircraft.ammo 
+		base_status = "HP: " + str(self.hp) +", Aircraft count: "+ str(len(self.aircrafts)) +", Ammo Storage: " + str(self.ammo) + ", Total damage: " + str(sum_of_damage)	
+		base_status += "Aircrafts: \n"
+		for aircraft in self.aircrafts:
+			base_status += aircraft.get_status() + "\n"
+		return base_status	
