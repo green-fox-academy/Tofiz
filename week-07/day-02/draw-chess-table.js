13be558132216727bd4ed8df454a693d11ea8b6d
Tofiz <tofi.zagon@gmail.com>
2017-10-24 16:53:32 +0200

    feat(draw-chess-table.js): done

---
+++ week-07/day-02/draw-chess-table.js
@@ -0,0 +1,16 @@
+// Crate a program that draws a chess table like this
+//
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % % 
+//  % % % %
+//
+
+for ( let i = 0; i <= 3; i ++) {
+    console.log("% % % %")
+    console.log(" % % % %")
+}
