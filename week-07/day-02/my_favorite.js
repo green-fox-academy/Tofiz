cb0cbe69e76659a334b1c8cc355565d3f3204633
Tofiz <tofi.zagon@gmail.com>
2017-10-24 13:59:55 +0200

    feat(my_favorite.js): done

---
+++ week-07/day-02/my_favorite.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var favoriteNumber = 8
+// Store your favorite number in a variable (as a number)
+// And print it like this: 'My favorite number is: 8'
+
+var my_num = 'My favorite number is: ' + 2;
+
+console.log(my_num)
