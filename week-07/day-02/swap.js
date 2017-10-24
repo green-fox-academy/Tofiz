21d9eaf5bd216fd70cb14cb629885d0630c04d70
Tofiz <tofi.zagon@gmail.com>
2017-10-24 14:02:34 +0200

    feat(swap.js): done

---
+++ week-07/day-02/swap.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Swap the values of these variables
+var a = 123;
+var b = 526;
+
+console.log(a);
+console.log(b);
+
+var swap = b;
+b = a;
+a = swap;
+
+console.log(a);
+console.log(b);
