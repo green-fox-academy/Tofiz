ad2af928fe244adfa491e082f56c94475e2d37a1
Tofiz <tofi.zagon@gmail.com>
2017-10-25 11:51:54 +0200

    feat(doubling.js): done

---
+++ week-07/day-03/doubling.js
@@ -0,0 +1,13 @@
+'use strict';
+// - Create a variable named `ak` and assign the value `123` to it
+// - Create a function called `doubling` that doubles it's input parameter
+// - Log the result of `doubling(ak)` to the console
+
+var ak = 123;
+
+function doubling() {
+    let d = ak * 2
+    return d
+}
+
+console.log(doubling(ak))
