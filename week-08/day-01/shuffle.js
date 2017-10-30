dc63e73492860c5e49a5ccb5cc15d7f31a0026cb
Tofiz <tofi.zagon@gmail.com>
2017-10-30 13:33:47 +0100

    feat(shuffle.js): done

--- week-08/day-01/shuffle.js
+++ week-08/day-01/shuffle.js
@@ -3,13 +3,33 @@ const Panama = {
     name: 'Panama',
     tax: '1%',
     deposit: function(amt) {
+        this.cash += amt;
+    }
+}
+
+const Cyprus = {
+    cash: 0,
+    name: 'Cyprus',
+    tax: '5%',
+    deposit: function(amt) {
+        this.cash += amt;
     }
 }
 
 const Shuffler = {
     cash: 10000,
+    shuffle_times: 0,
     pick: function() {
-    
+        this.cash -= 1000
+        let name = null;
+        if ( this.shuffle_times % 2 == 0) {
+            name = Panama;
+        } else {
+            name = Cyprus;
+        }
+        this.shuffle_times += 1;
+        name.deposit(1000);
+        console.log(name.name + " got 1000");
     }
 }
 
@@ -17,6 +37,7 @@ Shuffler.pick() // prints Panama got 1000
 Shuffler.pick() // prints Cyprus got 1000
 Shuffler.pick() // prints Panama got 1000
 Shuffler.pick() // prints Cyprus got 1000
+
 
 console.log( Panama.cash ) // 2000 
 console.log( Cyprus.cash ) // 2000 
