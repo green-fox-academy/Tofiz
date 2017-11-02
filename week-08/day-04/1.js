9f8f9e24e497c7bf013d01bbc4020f79be021d20
Tofiz <tofi.zagon@gmail.com>
2017-11-02 15:22:13 +0100

    feat(task1): on src click querySelector added

--- week-08/day-04/1.js
+++ week-08/day-04/1.js
@@ -1,28 +1,34 @@
 'use strict'
 
-
-
 function getGif (callback) {
-    let httpRequest = new XMLHttpRequest();
+    let httpRequest = new XMLHttpRequest()
     httpRequest.open('GET','http://api.giphy.com/v1/gifs/search?q=taylor+swift&api_key=YwfLefQAMvZEgmcKAX9scwAZzUT9vWAr&limit=16');
     httpRequest.send(null)
     httpRequest.onreadystatechange = function() {
         if(httpRequest.readyState == 4) {
             let gifs = JSON.parse(httpRequest.responseText).data
-            callback(gifs);
+            callback(gifs)
         }
     }
 }
 
 function displayData(data) {
     data.forEach(function(gif) {
-        let img = document.createElement("img");
-        let url = gif.images.downsized_still.url;
-        img.src = url;
-        body.appendChild(img);
-    });
+        let img = document.createElement("img")
+        let url = gif.images.downsized_still.url
+        let urlBig = gif.images.fixed_width.url
+        img.src = url
+        small.appendChild(img)
+        imageEventListener(img)
+    })
 }
 
-let body = document.querySelector("body")
-let a = getGif(displayData)
+function imageEventListener(img) {
+    img.addEventListener('click', function() {
+        
+    })
+}
 
+let big = document.querySelector(".big")
+let small = document.querySelector(".small")
+getGif(displayData)
