2a8890770e6135d9b752a1aa95ba9976726866f2
Tofiz <tofi.zagon@gmail.com>
2017-11-08 15:42:53 +0100

    feat(rest, backend project): doubling the input

--- week-09/day-03/index.js
+++ week-09/day-03/index.js
@@ -1,11 +1,32 @@
 var express = require('express');
 var app = express();
 
+express.json.type = "application/json"
+
 app.use('/assets', express.static('./assets'));
 
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
-  });
+});
   
-  app.listen(3000);
+
+
+app.get('/doubling', function(req, res){
+    let input = req.query.input;
+    if (input != null) {
+        res.send({
+            "received": input,
+            "result": input * 2
+        })
+    } else {
+        res.send({"error":"Please provide an input!"
+        })
+
+    }
+
+
+
+});
+
+    app.listen(8080);
